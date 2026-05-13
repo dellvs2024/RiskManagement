4b92e751a6fff71313db888ab2b8b61e19fd7a28
Shashank Kumar <dellvs2024@outlook.com>
2026-05-13 18:37:37 +0000

    changed component

--- app/risks/webapp/Component.js
+++ app/risks/webapp/Component.js
@@ -5,6 +5,7 @@ sap.ui.define(
 
         return Component.extend("riskmanagement.risks.Component", {
             metadata: {
+                
                 manifest: "json"
             }
         });
